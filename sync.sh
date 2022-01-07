84faf9d46d42226ec3ad45e8de66a97ad80659ba
phuzion <398094+phuzion@users.noreply.github.com>
2022-01-07 10:28:36 -0500

    Importing changes from Marlin directory

---
+++ sync.sh
@@ -0,0 +1,7 @@
+cp ../../Marlin/Marlin-2.0.9.3/Marlin/Configuration.h .
+cp ../../Marlin/Marlin-2.0.9.3/Marlin/Configuration_adv.h .
+cp ../../Marlin/Marlin-2.0.9.3/Marlin/_Bootscreen.h .
+cp ../../Marlin/Marlin-2.0.9.3/Marlin/_Statusscreen.h .
+git add *
+git commit -m "Importing changes from Marlin directory"
+git push
